ab91bc3103ca92dc63e3560f3d5908a7ff07bdc6
Madhur1893 <madhur.verma@crownstack.com>
2025-03-05 12:40:26 +0530

    Practice over few route handler method decorator and route handler parameter decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Body,
+  ParseIntPipe,
+  Headers,
+  Ip,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/:id/{:optional}')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param('optional') optional: number,
+    @Query('limit', ParseIntPipe) limit?: number,
+  ) {
+    console.log(id);
+    console.log(limit);
+    console.log(optional);
+    if (optional) {
+      return `ID is ${id} and optional parameter is ${optional}`;
+    } else {
+      return `ID is ${id} and no optional parameter`;
+    }
+  }
+
+  @Post()
+  public createUsers(
+    @Body('email') email: any,
+    @Headers() headers: any,
+    @Ip() Ip: any,
+  ) {
+    console.log(email);
+    console.log(headers);
+    console.log(Ip);
+    return 'You sent a post reques to users endpoints';
+  }
+}
