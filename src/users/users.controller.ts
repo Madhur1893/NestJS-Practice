d655b4e4d35ef81ed6eff09e7107cfbaa03683c1
Madhur1893 <madhur.verma@crownstack.com>
2025-03-06 16:32:51 +0530

    Practice on validation and pipes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,41 +2,51 @@ import {
   Controller,
   Get,
   Post,
+  Patch,
   Param,
   Query,
   Body,
   ParseIntPipe,
   Headers,
   Ip,
+  DefaultValuePipe,
 } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
+import { PatchUsersDto } from './dtos/patch-users.dto';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id/{:optional}')
+  @Get('{/:id}')
   public getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
-    @Param('optional') optional: number,
-    @Query('limit', ParseIntPipe) limit?: number,
+    @Param() getUsersParamDto: GetUsersParamDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(id);
+    console.log(typeof getUsersParamDto);
+    console.log(typeof limit);
+    console.log(typeof page);
+    console.log(getUsersParamDto);
     console.log(limit);
-    console.log(optional);
-    if (optional) {
-      return `ID is ${id} and optional parameter is ${optional}`;
-    } else {
-      return `ID is ${id} and no optional parameter`;
-    }
+    console.log(page);
+    return `You sent a get request to user endpoint`;
   }
 
   @Post()
   public createUsers(
-    @Body('email') email: any,
+    @Body() createUserDto: CreateUserDto,
     @Headers() headers: any,
     @Ip() Ip: any,
   ) {
-    console.log(email);
+    console.log(createUserDto instanceof CreateUserDto);
     console.log(headers);
     console.log(Ip);
-    return 'You sent a post reques to users endpoints';
+    return 'You sent a post request to users endpoints';
+  }
+
+  @Patch()
+  public patchUsers(@Body() patchUserDto: PatchUsersDto) {
+    console.log(patchUserDto);
+    return 'You sent a pacth request to users endpoints';
   }
 }
