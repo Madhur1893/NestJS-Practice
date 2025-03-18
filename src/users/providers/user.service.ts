fde64c94fe88c292f61c4f6151d14827877cd5d5
Madhur1893 <madhur.verma@crownstack.com>
2025-03-18 14:52:00 +0530

    Intalled PostgreSQL, created user entity and repository

--- src/users/providers/user.service.ts
+++ src/users/providers/user.service.ts
@@ -1,6 +1,10 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-users-param.dto';
 import { AuthService } from 'src/auth/providers/auth.service';
+import { Repository } from 'typeorm';
+import { User } from '../user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from '../dtos/create-user.dto';
 
 /**
  *  Class to connect to users table and perform buisness operation
@@ -8,10 +12,28 @@ import { AuthService } from 'src/auth/providers/auth.service';
 @Injectable()
 export class UserService {
   constructor(
+    // Injecting userRepository
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+
     //Injecting Auth Service
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
+
+  public async createUser(createUserDto: CreateUserDto) {
+    //Check if user exist with same email
+    const existingUser = await this.userRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    //Handling exception
+    //create a new user
+    let newUser = this.userRepository.create(createUserDto);
+    newUser = await this.userRepository.save(newUser);
+    return newUser;
+  }
+
   /**
    * The method to get all the users from the database
    */
